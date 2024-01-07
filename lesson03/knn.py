cb343e7283786265549efece7ee7e2fc97f5ccd5
Guangyou-Yu <yu.guang.you@163.com>
2024-01-07 10:21:33 +0800

    first ML

---
+++ lesson03/knn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+"""
+@author:ygy
+contact:1033207632@qq.com
+@file:knn.py
+@project:pycharm
+@time:2024/1/7  10:17
+"""
+
+class KNN(object):
+    def __init__(self):
+        pass
